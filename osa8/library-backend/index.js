89b8d3569aec932a49a0dcaaed00b14b8627522b
Pekka Laine <pekkaarvolaine@gmail.com>
2020-10-04 21:29:29 +0300

    8.2: kaikki kirjat ja kirjailijat

--- osa8/library-backend/index.js
+++ osa8/library-backend/index.js
@@ -86,6 +86,7 @@ let books = [
 const typeDefs = gql`
   type Book {
     title: String!
+    author: String!
     published: Int!
     id: ID!
     genres: [String!]!
@@ -93,10 +94,12 @@ const typeDefs = gql`
   type Author {
     name: String!
     born: Int
+    id:ID!
   }
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `
 
@@ -104,6 +107,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: () => books
   }
 }
 
