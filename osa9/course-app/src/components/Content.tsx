9b956750f06f82be005b50c41a67d8e194b693a8
Dicky59 <pekkaarvolaine@gmail.com>
2020-11-28 23:46:52 +0200

    Exercise 9.15.

--- osa9/course-app/src/components/Content.tsx
+++ osa9/course-app/src/components/Content.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import Part from './Part';
+import { CoursePart } from '../types';
 
-interface ContentProps {
-  name: string;
-  exerciseCount: number;
-}
-
-const Content: React.FC<{ courseParts: ContentProps[] }> = ({ courseParts }) => {
+const Content: React.FC<{
+  courseParts: CoursePart[];
+}> = ({ courseParts }) => {
   return (
     <div>
-      { courseParts.map(part => (<p key={part.name}>{part.name} {part.exerciseCount}</p>)) }
+      {courseParts.map((part, index) => (
+        <div key={index}>
+          <Part part={part} />
+        </div>
+      ))}
     </div>
-  )
-}
-
+  );
+};
 export default Content;
