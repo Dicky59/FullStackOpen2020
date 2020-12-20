173513d7437d793b3147b520b00775e1c00ba4b9
Dicky59 <pekkaarvolaine@gmail.com>
2020-12-21 01:17:51 +0200

    9.20: patientor step5

--- osa9/patientor-frontend/src/PatientListPage/PatientPage.tsx
+++ osa9/patientor-frontend/src/PatientListPage/PatientPage.tsx
@@ -1,31 +1,35 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
-import { Container, Icon } from 'semantic-ui-react';
+import { Container, Header, Icon } from 'semantic-ui-react';
 import { useParams } from 'react-router-dom';
 import { useStateValue, updatePatient } from '../state';
 import { apiBaseUrl } from '../constants';
-import { Patient, Gender } from '../types';
+import { Patient, Gender, Entry } from '../types';
 
 const PatientPage: React.FC = () => {
   const [{ patients }, dispatch] = useStateValue();
   const { id } = useParams<{ id: string }>();
-  let patient = patients[id];
+  const patient = patients[id];
 
   useEffect(() => {
-    if (patient && !patient.ssn) {
-      const fetchPatient = async () => {
-        try {
+    const fetchPatient = async () => {
+      try {
+        if (patient && !patient['ssn']) {
           const { data: patientFromApi } = await axios.get<Patient>(
-            `${apiBaseUrl}/api/patients/${patient.id}`
+            `${apiBaseUrl}/patients/${patient.id}`
           );
           dispatch(updatePatient(patientFromApi));
-        } catch (e) {
-          console.error(e);
         }
-      };
-      fetchPatient();
-    }
-  }, [id, patient, dispatch]);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    fetchPatient();
+  }, [dispatch, patient]);
+
+  if (!patient || !patient['ssn']) return (
+    <div>Loading patient information...</div>
+  );
 
   if (!patient) {
     return <div>No patient found</div>;
@@ -37,17 +41,71 @@ const PatientPage: React.FC = () => {
       ? "mars"
       : "venus mars";
 
-  return (
-    <div className="App">
-      <Container>
-        <h2>
-        {patient.name} <Icon name={genderIcon} />
-        </h2>
-        <p>ssn: {patient.ssn} </p>
-        <p>occupation: {patient.occupation} </p>
-        <p>Birth date: {patient.dateOfBirth}</p>
-      </Container>
-    </div>
+interface EntryDataProps {
+  entry: Entry;
+}
+
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discrimitated union member: ${JSON.stringify(value)}`
+  );
+};
+
+const EntryData: React.FC<EntryDataProps> = ({ entry }) => {
+  switch (entry.type) {
+    case 'HealthCheck':
+      return (
+        <div>
+          <p>{entry.date} <span style={{fontStyle: 'italic'}}>{entry.description}</span></p>
+          <ul>
+            {entry.diagnosisCodes?.map(code =>
+              <li key={code}>{code}</li>
+            )}
+          </ul>
+        </div>
+      );
+    case 'OccupationalHealthcare':
+      return (
+        <div>
+          <p>{entry.date} <span style={{fontStyle: 'italic'}}>{entry.description}</span></p>
+          <ul>
+            {entry.diagnosisCodes?.map(code =>
+              <li key={code}>{code}</li>
+            )}
+          </ul>
+        </div>
+      );
+    case 'Hospital':
+     return (
+        <div>
+          <p>{entry.date} <span style={{fontStyle: 'italic'}}>{entry.description}</span></p>
+          <ul>
+            {entry.diagnosisCodes?.map(code =>
+              <li key={code}>{code}</li>
+            )}
+          </ul>
+       </div>
+     );
+     default:
+      return assertNever(entry);
+  }
+};
+
+return (
+  <Container>
+    <Header as='h2'>
+      {patient.name}
+      <Icon name={genderIcon} />
+    </Header>
+
+    <div>ssn: {patient.ssn}</div>
+    <div>occupation: {patient.occupation}</div>
+
+    <Header as='h3'>entries</Header>
+    {patient.entries?.map(entry =>
+      <EntryData key={entry.id} entry={entry}/>
+    )}
+  </Container>
   );
 };
 
