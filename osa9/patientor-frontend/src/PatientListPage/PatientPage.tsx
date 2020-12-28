9771c7e417513ac696c67af17d0d7407df814695
Dicky59 <pekkaarvolaine@gmail.com>
2020-12-29 01:31:53 +0200

    9.24: patientor, step9

--- osa9/patientor-frontend/src/PatientListPage/PatientPage.tsx
+++ osa9/patientor-frontend/src/PatientListPage/PatientPage.tsx
@@ -1,17 +1,45 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
-import { Container, Header } from 'semantic-ui-react';
+import { Container, Header, Button } from 'semantic-ui-react';
 import { useParams } from 'react-router-dom';
 import { useStateValue, updatePatient } from '../state';
 import { apiBaseUrl } from '../constants';
 import { Patient } from '../types';
 import EntryDetails from './EntryDetails';
 import GenderIcon from './GenderIcon';
+import AddEntryModal from '../AddEntryModal';
+import { EntryFormValues } from '../AddEntryModal/AddEntryForm';
+
 
 const PatientPage: React.FC = () => {
   const [{ patients }, dispatch] = useStateValue();
   const { id } = useParams<{ id: string }>();
   const patient = patients[id];
+
+  const [modalOpen, setModalOpen] = React.useState<boolean>(false);
+  const [error, setError] = React.useState<string | undefined>();
+
+  const openModal = (): void => setModalOpen(true);
+
+  const closeModal = (): void => {
+    setModalOpen(false);
+    setError(undefined);
+  };
+
+  const submitNewPatient = async (values: EntryFormValues) => {
+    try {
+      const { data: updatedPatient } = await axios.post<Patient>(
+        `${apiBaseUrl}/patients/${id}/entries`,
+        values
+      );
+      if (!updatePatient) throw new Error(`Patient with id ${id} not found`);
+      dispatch(updatePatient(updatedPatient));
+      closeModal();
+    } catch (e) {
+      console.error(e.response.data);
+      setError(e.response.data.error);
+    }
+  };
 
   useEffect(() => {
     const fetchPatient = async () => {
@@ -37,8 +65,6 @@ const PatientPage: React.FC = () => {
     return <div>No patient found</div>;
   }
 
-
-
 return (
   <Container>
     <Header as='h2'>
@@ -49,7 +75,16 @@ return (
     <div>ssn: {patient.ssn}</div>
     <div>occupation: {patient.occupation}</div>
 
-    <Header as='h3'>entries</Header>
+    <Header as='h3'>Entries</Header>
+
+    <AddEntryModal
+        modalOpen={modalOpen}
+        onSubmit={submitNewPatient}
+        error={error}
+        onClose={closeModal}
+      />
+      <Button onClick={() => openModal()}>Add New Entry</Button>
+      
     {patient.entries?.map(entry =>
       <EntryDetails key={entry.id} entry={entry}/>
     )}
